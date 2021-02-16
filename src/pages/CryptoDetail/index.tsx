4a44182b7c1a5f2125dddc98721de268fb08723d
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2021-02-16 07:30:56 -0300

    Create HeaderBar to CrypoDetail screen

--- src/pages/CryptoDetail/index.tsx
+++ src/pages/CryptoDetail/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { HeaderBar } from '../../components';
+ 
+import { dummyData, COLORS, FONTS, SIZES, icons } from '../../constants';
 
-const CryptoDetail = () => {
+const CryptoDetail = ({ navigation }) => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>CryptoDetail</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightGray1}}>
+      <HeaderBar right={true}/>
+    </SafeAreaView>
   );
 };
 export default CryptoDetail;
